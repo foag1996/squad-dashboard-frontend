4e0caa994c9b3b1ecd92d07fec6dea33497d358c
Fabian Alvarez <62860097+foag1996@users.noreply.github.com>
2026-04-21 20:39:06 -0500

    feat: add app config and routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./app.component').then(m => m.AppComponent)
+  }
+];
